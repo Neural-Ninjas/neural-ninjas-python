8bd64965961285402d345d5bd70c1875d9b9aa38
maxmoeschinger <max@birdsview.no>
2023-05-03 21:32:27 +0200

    start work on logging

--- neural_ninjas/api/neural_ninjas.py
+++ neural_ninjas/api/neural_ninjas.py
@@ -3,3 +3,16 @@ import os
 class NeuralNinjas:
     def __init__(self):
         self.api_key = os.environ['NEURAL_NINJAS_API_KEY']
+    
+    def init(self, project):
+        # Make API call to Neural Ninjas
+        # Return run_id
+        pass
+
+    def log_metrics(self, metrics: dict):
+        # Make API call to Neural Ninjas
+        pass
+
+    def log_artifact(self, type: str, path: str):
+        # Make API call to Neural Ninjas
+        pass
